8735bf21b97366a8854eada3ad656a2a958a4b79
kevinl <kevinliu987@gmail.com>
2016-07-07 16:20:47 -0700

    content js

---
+++ content.js
@@ -0,0 +1,5 @@
+// alert("hello");
+// document.body.style.background = 'yellow';
+document.body.style.backgroundImage= "url('http://placekitten.com/200/300')"
+
+document.body.style.backgroundRepeat= "no-repeat"
