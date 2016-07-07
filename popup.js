2de7d8dea96973520b18040b2428e5a5e402abb6
kevinl <kevinliu987@gmail.com>
2016-07-07 14:28:59 -0700

    update

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ chrome.contextMenus.create({title: "What's the urbandictionary definition of '%s
 														// Need brackets on selection to grab selected word on Chrome
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "http://www.urbandictionary.com/define.php?term="+encodeURIComponent(info.selectionText);
+														let link = "http://www.urbandictionary.com/define.php?term="+encodeURIComponent(info.selectionText)+"?id=helpushelpyou";
 														chrome.windows.create({url: link});
 													}
 });
@@ -10,7 +10,7 @@ chrome.contextMenus.create({title: "What's the urbandictionary definition of '%s
 chrome.contextMenus.create({title: "What's the standard definition of '%s'",
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "http://www.dictionary.com/browse/"+encodeURIComponent(info.selectionText);
+														let link = "http://www.dictionary.com/browse/"+encodeURIComponent(info.selectionText)+"?id=helpushelpyou";
 														chrome.windows.create({url: link});
 													}
 });
@@ -18,7 +18,7 @@ chrome.contextMenus.create({title: "What's the standard definition of '%s'",
 chrome.contextMenus.create({title: "Discover the story of '%s' on Wikipedia",
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "https://en.wikipedia.org/wiki/"+encodeURIComponent(info.selectionText);
+														let link = "https://en.wikipedia.org/wiki/"+encodeURIComponent(info.selectionText)+"?id=helpushelpyou";
 														chrome.windows.create({url: link});
 													}
 });
@@ -26,7 +26,7 @@ chrome.contextMenus.create({title: "Discover the story of '%s' on Wikipedia",
 chrome.contextMenus.create({title: "How do you pronounce '%s' in Chinese",
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "https://translate.google.com/?ion=1&espv=2&bav=on.2,or.r_cp.&bvm=bv.126130881,d.cGc&biw=1855&bih=951&dpr=1&um=1&ie=UTF-8&hl=en&client=tw-ob#en/zh-CN/"+encodeURIComponent(info.selectionText);
+														let link = "https://translate.google.com/?ion=1&espv=2&bav=on.2,or.r_cp.&bvm=bv.126130881,d.cGc&biw=1855&bih=951&dpr=1&um=1&ie=UTF-8&hl=en&client=tw-ob#en/zh-CN/"+encodeURIComponent(info.selectionText)+"?id=helpushelpyou";
 														chrome.windows.create({url: link});
 													}
 });
@@ -34,7 +34,7 @@ chrome.contextMenus.create({title: "How do you pronounce '%s' in Chinese",
 chrome.contextMenus.create({title: "Show me some pictures of '%s'",
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "https://www.google.com/search?noj=1&site=imghp&tbm=isch&source=hp&biw=1855&bih=990&q="+encodeURIComponent(info.selectionText)
+														let link = "https://www.google.com/search?noj=1&site=imghp&tbm=isch&source=hp&biw=1855&bih=990&q="+encodeURIComponent(info.selectionText)+"?id=helpushelpyou"
 														chrome.windows.create({url: link});
 													}
 });
@@ -42,7 +42,7 @@ chrome.contextMenus.create({title: "Show me some pictures of '%s'",
 chrome.contextMenus.create({title: "I want to buy '%s'",
 														contexts:["selection"],
 														onclick: function(info){
-														let link = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+encodeURIComponent(info.selectionText)
+														let link = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+encodeURIComponent(info.selectionText)+"?id=helpushelpyou"
 														chrome.windows.create({url: link});
 													}
 });
