dd5c305a6381294368f71883dcec4f91f8fb9a34
kevinl <kevinliu987@gmail.com>
2016-07-07 02:20:53 -0700

    update

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@
 
 // //Currently this simply checks for which menu item was clicked.
 //  if (info.menuItemId == "main_parent") {
-//    chrome.tabs.create({ 
+//    chrome.tabs.create({
 //       url: "https://www.google.com/search?q=" + info.selectionText,
 //    })
 //  }
@@ -32,7 +32,7 @@
 
 
 // var work = function() {
-// 	console.log(window.getSelection().toString());	
+// 	console.log(window.getSelection().toString());
 // }
 
 // document.body.addEventListener('dblclick', work);
@@ -40,34 +40,42 @@
 
 
 // Create selection context
-var title = "Search Urban Dictionary for 'selection'";
-var id = chrome.contextMenus.create({"title": title, "contexts":["selection"], "onclick": genericOnClick});
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-	var selectedWord = info.selectionText;
-	// not able to add to url
-	// var selectedWord = window.getSelection().toString();
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.tabs.executeScript(tab.id,
+//       {code: 'window.getSelection().toString()'}, function(results) {
+//     alert(results[0]);
+//   });
+// });
 
-	// googled encodeURIComponent
-	var urbanLink = "http://www.urbandictionary.com/define.php?term=" + encodeURIComponent(selectedWord);
-	// Open the new tab with the search query
-	chrome.windows.create({ url: urbanLink});
-}
-
-// Update the contextMenus title
-function updateContextMenu(id) {
-	// Update contextMenus with highlighted selection
-	chrome.contextMenus.update(id, {"title": "Search Urban Dictionary for '%s'"});
-}
-
-// Updates the contextMenus title each time the menu is opened.
-updateContextMenu(id);
-
-
-
-
-
-
-
-
+chrome.contextMenus.create({title: "Let's Urbandctionary it : '%s' ",
+														contexts:["selection"],
+														onclick: function(info){
+														let link = "http://www.urbandictionary.com/define.php?term="+encodeURIComponent(info.selectionText);
+														chrome.windows.create({url: link});
+													}
+});
+//
+// var title = "Search Urban Dictionary for 'selection'";
+// var id = chrome.contextMenus.create({"title": title, "contexts":["selection"], "onclick": genericOnClick});
+//
+// // A generic onclick callback function.
+// function genericOnClick(info, tab) {
+// 	var selectedWord = info.selectionText;
+// 	// not able to add to url
+// 	// var selectedWord = window.getSelection().toString();
+//
+// 	// googled encodeURIComponent
+// 	var urbanLink = "http://www.urbandictionary.com/define.php?term=" + encodeURIComponent(selectedWord);
+// 	// Open the new tab with the search query
+// 	chrome.windows.create({ url: urbanLink});
+// }
+//
+// // Update the contextMenus title
+// function updateContextMenu(id) {
+// 	// Update contextMenus with highlighted selection
+// 	chrome.contextMenus.update(id, {"title": "Search Urban Dictionary for '%s'"});
+// }
+//
+// // Updates the contextMenus title each time the menu is opened.
+// updateContextMenu(id);
